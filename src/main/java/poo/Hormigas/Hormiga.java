23e9c883db6025b777de02c6c8198690f7d5c0e4
josue <josuecevallos.g@gmail.com>
2023-04-27 00:28:42 +0200

    Nuevas funciones: getID -> dependiendo del tipo de hormiga -> string con la identificación de la hormiga. Se apoya de la funcion formato() ! formato -> dependiendo del numero de digitos devuelve un formato u otro

--- src/main/java/poo/Hormigas/Hormiga.java
+++ src/main/java/poo/Hormigas/Hormiga.java
@@ -31,6 +31,9 @@ public class Hormiga extends Thread{
                 //hormiga IMPAR
                 c.entraColonia(this);
                 c.saleColonia(this, eligeSalida());
+                c.recogeAlimentos(this);
+                c.entraColonia(this);
+                
                 
                 //HORMIGA PAR
                 
@@ -45,10 +48,48 @@ public class Hormiga extends Thread{
                 System.out.println("tipo no valido");
         }
     }
-
+    /*
     public int getMiId() {
         
         return this.id;
+    }*/
+    
+    public String getMiId(String tipo) {
+        String str = "";
+        char letra;
+        if(tipo=="obrera"){
+            letra = 'O';
+            str = formato(letra);
+        } else if(tipo=="soldado"){
+            letra = 'S';
+            str = formato(letra); 
+        } else {
+            letra = 'C';
+            str = formato(letra); 
+        }
+        
+        return str;
+    }
+    
+    public String formato(char letra){
+        
+        String str="";
+        if (id < 10) {
+            str = "H"+letra+"0000"+id;
+        } else if (id < 100) {
+            str = "H" +letra+"000"+id;
+        } else if (id < 1000) {
+            str = "H"+letra+"00"+id;
+        } else if (id < 10000) {
+            str = "H"+letra+"0"+id;
+        } else {
+            str = "H"+letra+ id;
+        }
+        return str;
+    }
+    
+    public String getTipo(){
+        return this.tipo;
     }
     
     public int eligeSalida(){
